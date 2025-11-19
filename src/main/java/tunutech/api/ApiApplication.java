a3f6b9f37e1b3f8dfbd00116ae16959e5c32acf7
Genhl <levihaoudou@gmail.com>
2025-11-19 09:33:19 +0000

    activitydsdsd ddd

--- src/main/java/tunutech/api/ApiApplication.java
+++ src/main/java/tunutech/api/ApiApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("tunutech.api.repositories")
-@EntityScan("tunutech.api.model")  // ← CHANGEZ ICI
 public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
